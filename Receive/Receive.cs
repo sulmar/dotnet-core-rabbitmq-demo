40e52cc1b7eced177696661c0f50fff1f0c4aee8
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2019-03-08 19:05:03 +0100

    dodanie przykladow

--- Receive/Receive.cs
+++ Receive/Receive.cs
@@ -1,23 +1,121 @@
 ﻿using System;
 using System.Text;
+using Newtonsoft.Json;
 using RabbitMQ.Client;
 using RabbitMQ.Client.Events;
 
 namespace Receive
 {
+     public class Order 
+    {
+        public DateTime OrderDate { get; set; }
+        public string CustomerNumber { get; set; }
+
+        public Order()
+        {
+            OrderDate = DateTime.Now;
+
+        }
+        public Order(string customerNumber)
+        : this()
+        {
+            this.CustomerNumber = customerNumber;
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
 
+           // ConsumerTest();
+
+
+            ConsumeOrder();
+
+         //  DirectTest();
+
+         // HeadersTest();
+
+
+        }
+
+        private static void ConsumeOrder()
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost"};
+
+            using(var connection = factory.CreateConnection())
+            using(var model = connection.CreateModel())
+            {
+                string queueName = "orders-queue";
+
+                model.QueueDeclare(queue: queueName,
+                durable: true,
+                exclusive: false,
+                autoDelete: false,
+                arguments: null);
+
+
+                var  consumer = new OrderConsumer(model);
+
+                consumer.OnReceive(order => System.Console.WriteLine($"Send {order.CustomerNumber}"));
+
+                // var consumer = new EventingBasicConsumer(channel);
+                // consumer.Received += (model, ea) => 
+                // {
+                //     var message =  Encoding.UTF8.GetString(ea.Body);
+
+                //     System.Console.WriteLine($"Received order {message}");
+
+                //     Order order = JsonConvert.DeserializeObject<Order>(message);
+
+                //     System.Console.WriteLine($"Send order {order.CustomerNumber} to <marcin.sulecki@sulmar.pl>");
+                // };
+
+                model.BasicConsume(queue: queueName,
+                autoAck: true,
+                consumer: consumer);
+
+                Console.WriteLine(" Press any to exit.");
+                Console.ReadKey();
+               
+            }
+        }
+
+        static void ConsumerTest()
+        {
             var factory = new ConnectionFactory() { HostName = "localhost"};
 
             using(var connection = factory.CreateConnection())
             using(var channel = connection.CreateModel())
             {
-                channel.QueueDeclare(queue: "hello",
-                durable: false,
+                channel.QueueDeclare(queue: "my-queue",
+                durable: true,
+                exclusive: false,
+                autoDelete: false,
+                arguments: null);
+
+                 MyConsumer consumer = new MyConsumer(channel);
+
+                channel.BasicConsume(queue: "my-queue",
+                autoAck: true,
+                consumer: consumer);
+
+                Console.WriteLine(" Press [enter] to exit.");
+                Console.ReadLine();
+            }
+
+        }
+
+        static void DirectTest()
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost"};
+
+            using(var connection = factory.CreateConnection())
+            using(var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "my-queue",
+                durable: true,
                 exclusive: false,
                 autoDelete: false,
                 arguments: null);
@@ -29,15 +127,116 @@ namespace Receive
                     System.Console.WriteLine($"Received {message}");
                 };
 
-                channel.BasicConsume(queue: "hello",
+                channel.BasicConsume(queue: "my-queue",
                 autoAck: true,
                 consumer: consumer);
 
-                    Console.WriteLine(" Press [enter] to exit.");
-                  Console.ReadLine();
+                Console.WriteLine(" Press [enter] to exit.");
+                Console.ReadLine();
                
             }
+        }
 
+        static void HeadersTest()
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost"};
+
+            using(var connection = factory.CreateConnection())
+            using(var channel = connection.CreateModel())
+            {
+                // channel.QueueDeclare(queue: "ReportPDF",
+                // durable: true,
+                // exclusive: false,
+                // autoDelete: false,
+                // arguments: null);
+
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += (model, ea) => 
+                {
+                    var message =  Encoding.UTF8.GetString(ea.Body);
+                    System.Console.WriteLine($"Received {message}");
+                };
+
+                channel.BasicConsume(queue: "ReportPDF",
+                autoAck: false,
+                consumer: consumer);
+
+                Console.WriteLine(" Press [enter] to exit.");
+                Console.ReadLine();
+               
+            }
+        }
+
+    }
+
+    public class OrderConsumer : EntityConsumer<Order>
+    {
+        public OrderConsumer(IModel model) 
+        : base(model)
+        {
+        }
+    }
+
+    public class EntityConsumer<T> : DefaultBasicConsumer
+    {
+        private readonly IModel model;
+        private Action<T> execute;
+
+        public void OnReceive(Action<T> execute)
+        {
+            this.execute = execute;
+        }
+
+        public EntityConsumer(IModel model) : base(model)
+        {
+            this.model = model;
+        }
+
+
+        public override void HandleBasicDeliver(string consumerTag, ulong deliveryTag, bool redelivered, string exchange, string routingKey, IBasicProperties properties, byte[] body)
+        {
+            Console.WriteLine($"Consuming Headers Message");
+            Console.WriteLine(string.Concat("Message received from the exchange ", exchange));
+            Console.WriteLine(string.Concat("Consumer tag: ", consumerTag));
+            Console.WriteLine(string.Concat("Delivery tag: ", deliveryTag));
+            Console.WriteLine(string.Concat("Routing tag: ", routingKey));
+        
+             var message =  Encoding.UTF8.GetString(body);
+
+            System.Console.WriteLine($"Received order {message}");
+
+            T entity = JsonConvert.DeserializeObject<T>(message);
+
+            execute?.Invoke(entity);
+
+           // Order order = JsonConvert.DeserializeObject<Order>(message);
+
+            // System.Console.WriteLine($"Send order {order.CustomerNumber} to <marcin.sulecki@sulmar.pl>");
+
+            model.BasicAck(deliveryTag, false);
+        }
+
+    }
+
+    public class MyConsumer : DefaultBasicConsumer
+    {
+        private readonly IModel model;
+
+        public MyConsumer(IModel model) : base(model)
+        {
+            this.model = model;
+        }
+
+
+        public override void HandleBasicDeliver(string consumerTag, ulong deliveryTag, bool redelivered, string exchange, string routingKey, IBasicProperties properties, byte[] body)
+        {
+            Console.WriteLine($"Consuming Headers Message");
+            Console.WriteLine(string.Concat("Message received from the exchange ", exchange));
+            Console.WriteLine(string.Concat("Consumer tag: ", consumerTag));
+            Console.WriteLine(string.Concat("Delivery tag: ", deliveryTag));
+            Console.WriteLine(string.Concat("Routing tag: ", routingKey));
+            Console.WriteLine(string.Concat("Message: ", Encoding.UTF8.GetString(body)));
+            model.BasicAck(deliveryTag, false);
 
         }
     }
--- Send/Send.cs
+++ Send/Send.cs
@@ -1,27 +1,200 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Text;
 using System.Threading;
+using Newtonsoft.Json;
 using RabbitMQ.Client;
 
 namespace Send
 {
+
+    class Order 
+    {
+        public DateTime OrderDate { get; set; }
+        public string CustomerNumber { get; set; }
+
+        public Order()
+        {
+            OrderDate = DateTime.Now;
+
+        }
+        public Order(string customerNumber)
+        : this()
+        {
+            this.CustomerNumber = customerNumber;
+        }
+    }
+
+    
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+           
+         //   HelloTest("Hello RabbitMQ!");
 
+          //  TTLTest();
+
+         //   HeadersTest();
+
+            Order order = new Order("1000");
+
+            SendOrder(order);
+
+         //  DirectTest();
+        }
+
+
+        private static void TTLTest()
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost"};
+
+            using(var connection = factory.CreateConnection())
+            using(var model = connection.CreateModel())
+            {
+               // var args = new Dictionary<string, object>();
+               // int ttl = (int) TimeSpan.FromMinutes(1).TotalMilliseconds;
+             //   args.Add("x-message-ttl", ttl);
+
+             
+
+             //   model.QueueDeclare(queue: "my-queue", durable: false, exclusive: false, autoDelete: false, arguments: null);
+
+                var message = "Hello TTL 2";
+
+                 var body = Encoding.UTF8.GetBytes(message);
+
+                    model.BasicPublish(exchange: string.Empty,
+                    routingKey: "mazda",
+                    body: body);
+
+                    System.Console.WriteLine($"Sent {message}");
+
+                    System.Console.WriteLine("Press any key to exit.");
+
+                    Console.ReadKey();
+            }
+
+        }
+
+
+        private static void HeadersTest()
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost"};
+
+            using(var connection = factory.CreateConnection())
+            using(var model = connection.CreateModel())
+            {
+                var headers = new Dictionary<string, object>();
+                headers.Add("x-match", "all");
+                headers.Add("format", "pdf");
+                headers.Add("color", "red");
+
+                string exchangeName = "headers.exchange";
+                string queueName = "ReportPDF";
+
+                model.ExchangeDeclare(exchange: exchangeName, type: ExchangeType.Headers);
+                model.QueueDeclare(queue: queueName, durable: true, autoDelete: false, exclusive: false);
+                model.QueueBind(queueName, exchangeName, string.Empty, headers);
+
+                int i=0;
+
+                while(true)
+                {
+                    string message = "File PDF #" + i++;
+
+                    var properties = model.CreateBasicProperties();
+                    properties.Headers = new Dictionary<string, object>();
+                    properties.Headers.Add("format", "pdf");
+                    properties.Headers.Add("color", "blue");
+
+                    var body = Encoding.UTF8.GetBytes(message);
+
+                    model.BasicPublish(exchange:exchangeName,
+                    routingKey:string.Empty,
+                    basicProperties: properties,
+                    body: body);
+
+                    System.Console.WriteLine($"Sent {message}");
+
+                    Thread.Sleep(TimeSpan.FromMilliseconds(100));
+                }
+
+            }
+        }
+
+
+        private static void SendOrder(Order order)
+        {
             var factory = new ConnectionFactory() { HostName = "localhost"};
 
             using(var connection = factory.CreateConnection())
             using(var channel = connection.CreateModel())
             {
-                channel.QueueDeclare(queue:"hello",
-                                    durable: false,
+                channel.QueueDeclare(queue:"orders-queue",
+                                    durable: true,
                                     exclusive: false,
                                     autoDelete: false,
                                     arguments: null
                 );
+
+                // add package Newtonsoft.Json
+                var message = JsonConvert.SerializeObject(order);
+
+                var body = Encoding.UTF8.GetBytes(message);
+
+                channel.BasicPublish(exchange: "",
+                routingKey:"orders-queue",
+                basicProperties: null,
+                body: body);
+
+                System.Console.WriteLine($"Sent {message}");
+                
+            }
+        }
+
+        private static void HelloTest(string message)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost"};
+
+            using(var connection = factory.CreateConnection())
+            using(var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue:"my-queue",
+                                    durable: true,
+                                    exclusive: false,
+                                    autoDelete: false,
+                                    arguments: null
+                );
+
+                var body = Encoding.UTF8.GetBytes(message);
+
+                channel.BasicPublish(exchange: "",
+                routingKey:"my-queue",
+                basicProperties: null,
+                body: body);
+
+                System.Console.WriteLine($"Sent {message}");
+
+                
+            }
+            
+        }
+
+        private static void DirectTest()
+        {
+              var factory = new ConnectionFactory() { HostName = "localhost"};
+
+            using(var connection = factory.CreateConnection())
+            using(var channel = connection.CreateModel())
+            {
+                // channel.QueueDeclare(queue:"hello",
+                //                     durable: false,
+                //                     exclusive: false,
+                //                     autoDelete: false,
+                //                     arguments: null
+                // );
 
                 int i=0;
 
